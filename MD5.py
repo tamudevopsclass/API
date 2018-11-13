d652304506ab8b5de6c28a130deb806d54df7135
TylerGrimm2019 <43386889+TylerGrimm2019@users.noreply.github.com>
2018-11-12 19:16:40 -0600

    Updateed md5 portion
    forgot to put in the update portion of the code

--- MD5.py
+++ MD5.py
@@ -1,9 +1,7 @@
 
-@app.route('/md5/<input>')
-def md5(input):
-    if input:
-      out = md5.new(input).digest()
-      return json.dumps({"input":input, "output":out})
-    else 
-     return "Error: input a string"
-
+@app.route('/md5/<path:inp>')
+def hash(inp):
+    m = md5.new()
+    m.update(inp.encode('utf-8'))
+    out = m.hexdigest()
+    return json.dumps({"input":inp, "output":out})
